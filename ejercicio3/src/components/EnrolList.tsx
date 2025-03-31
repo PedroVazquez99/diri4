87f1e2cdcc93445e86401237036e40ce3804cd39
pvt12 <pvt12@gcloud.ua.es>
2025-03-31 20:12:26 +0200

    Eliminar estilos innecesarios

--- ejercicio3/src/components/EnrolList.tsx
+++ ejercicio3/src/components/EnrolList.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-import "../styles/EnrolList.css";
 import { DetailsList } from "@fluentui/react/lib/DetailsList"; // Corregido el typo en @luentui
 import { initializeIcons } from "@fluentui/react/lib/Icons";
 import { Student } from "../entities/Student";
